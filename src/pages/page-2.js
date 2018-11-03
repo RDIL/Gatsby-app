37111f3c665bf9b914d6745124188509b8bdef14
RDIL <34555510+RDIL@users.noreply.github.com>
2018-11-03 17:11:08 -0400

    Create page-2.js

---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Hello from the second page!</h1>
+    <p><i>Welcome to page 2</i></p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
